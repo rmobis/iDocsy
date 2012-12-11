cb5235f8f801b8e135d37e7d332b4175b5ac31e1
Raphael Mobis Tacla <r.mobis@gmail.com>
2012-12-11 17:03:01 -0200

    Lazy-load items from Section model.

--- application/models/docsection.php
+++ application/models/docsection.php
@@ -7,7 +7,14 @@ class DocSection extends Eloquent {
 		return $this->belongs_to('DocModule');
 	}
 
-	public function items() {
-		return $this->has_many('DocItem', 'section_id')->order_by('name');
+	public function items($full = false) {
+		if ($full) {
+			return $this->has_many('DocItem', 'section_id')
+						->order_by('name');
+		} else {
+			return $this->has_many('DocItem', 'section_id')
+						->order_by('name')
+						->select(array('name', 'link'));
+		}
 	}
 }
