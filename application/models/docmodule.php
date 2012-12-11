aa72349c8730b5ba314491dc5781301cde27e9a8
Raphael Mobis Tacla <r.mobis@gmail.com>
2012-12-11 17:03:35 -0200

    Indenting fix.

--- application/models/docmodule.php
+++ application/models/docmodule.php
@@ -4,6 +4,7 @@ class DocModule extends Eloquent {
 	public static $table = 'modules';
 
 	public function sections() {
-		return $this->has_many('DocSection', 'module_id')->order_by('id');
+		return $this->has_many('DocSection', 'module_id')
+					->order_by('id');
 	}
 }
