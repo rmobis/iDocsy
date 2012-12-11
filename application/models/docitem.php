10a6e47fd6c5ccd785ec8118a11f6c6c0f8ca52b
Raphael Mobis Tacla <r.mobis@gmail.com>
2012-12-11 17:03:25 -0200

    Established Items and Parameters/Return Values relationships.

--- application/models/docitem.php
+++ application/models/docitem.php
@@ -6,4 +6,13 @@ class DocItem extends Eloquent {
 	public function section() {
 		return $this->belongs_to('DocSection');
 	}
+
+	public function return_value() {
+		return $this->has_one('ReturnValue', 'item_id');
+	}
+
+	public function parameters() {
+		return $this->has_many('Parameter', 'item_id')
+					->order_by('id');
+	}
 }
