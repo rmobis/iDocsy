d6508fddf5206369458513886279a92614a775f5
Raphael Mobis Tacla <r.mobis@gmail.com>
2012-12-11 20:47:13 -0200

    Set up eager-loading for new db structure.

--- application/controllers/docs.php
+++ application/controllers/docs.php
@@ -2,7 +2,7 @@
 
 class Docs_Controller extends Base_Controller {
 	public function action_index() {
-		$first_item = DocItem::with(array('section', 'section.module'))
+		$first_item = DocItem::with(array('section', 'section.module', 'data', 'parameters', 'return_value'))
 							 ->first();
 
 		return View::make('layouts.main')
