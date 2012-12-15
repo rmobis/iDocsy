1f976ecf864311b965da0598a7f07e123ba09204
Raphael Mobis Tacla <r.mobis@gmail.com>
2012-12-15 14:13:51 -0200

    Refactored database. (Again)
    This shit is getting annoying, I hope I'm finally satisfied with the
    database. Getting tired of redoing models and shit.

--- script.sql
+++ script.sql
@@ -1,59 +1,49 @@
 CREATE TABLE `modules` (
-	`id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	`name` VARCHAR(128) NOT NULL
-);
+	`id`	INT(10)		UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	`name`	VARCHAR(64)	NOT NULL,
+	`link`	VARCHAR(64) NOT NULL,
 
-CREATE TABLE `sections` (
-	`id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	`module_id` INT (10) UNSIGNED NOT NULL,
-	`name` VARCHAR(128) NOT NULL,
-
-	INDEX (`module_id`),
-	FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`)
+	INDEX (`link`)
 );
 
 CREATE TABLE `items` (
-	`id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	`section_id` INT (10) UNSIGNED NOT NULL,
-	`name` VARCHAR(128) NOT NULL,
-	`link` VARCHAR(128) NOT NULL UNIQUE KEY,
+	`id`		INT(10)				UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	`module_id`	INT(10)				UNSIGNED NOT NULL,
+	`type`		ENUM(
+					'Variable',
+					'Function',
+					'Constant',
+					'Type'
+				)					NOT NULL,
+	`name`		VARCHAR(64) 		NOT NULL,
+	`link`		VARCHAR(64) 		NOT NULL,
 
-	INDEX (`link`),
-	INDEX (`section_id`),
-	FOREIGN KEY (`section_id`) REFERENCES `sections` (`id`)
+	UNIQUE	(`link`, `module_id`),
+	INDEX	(`link`, `module_id`),
+	INDEX	(`module_id`),
+	FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`)
 );
 
-CREATE TABLE `item_data` (
-	`id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	`item_id` INT(10) UNSIGNED NOT NULL,
-	`html_desc` VARCHAR(2048) NOT NULL,
-	`bbcd_desc` VARCHAR(2048) NOT NULL,
-	`html_usage` VARCHAR(1024) NOT NULL,
-	`bbcd_usage` VARCHAR(1024) NOT NULL,
+CREATE TABLE `headings` (
+	`id`		INT(10)			UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	`item_id`	INT(10)			UNSIGNED NOT NULL,
+	`order`		TINYINT(2)		UNSIGNED NOT NULL,
+	`name`		VARCHAR(128)	NOT NULL,
+	`html_desc`	VARCHAR(1024)	NOT NULL,
+	`bbcd_desc`	VARCHAR(1024),
 
+	UNIQUE (`item_id`, `order`),
 	FOREIGN KEY (`item_id`) REFERENCES `items` (`id`)
 );
 
-CREATE TABLE `return_values` (
-	`id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	`item_id` INT(10) UNSIGNED NOT NULL,
-	`type` VARCHAR(64) NOT NULL,
-	`html_desc` VARCHAR(256) NOT NULL,
-	`bbcd_desc` VARCHAR(256) NOT NULL,
+CREATE TABLE `sub_headings` (
+	`id`			INT(10)			UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	`heading_id`	INT(10)			UNSIGNED NOT NULL,
+	`order`			TINYINT(2)		UNSIGNED NOT NULL,
+	`name`			VARCHAR(128)	NOT NULL,
+	`html_desc`		VARCHAR(512)	NOT NULL,
+	`bbcd_desc`		VARCHAR(512),
 
-	INDEX (`item_id`),
-	FOREIGN KEY (`item_id`) REFERENCES `items` (`id`)
-);
-
-CREATE TABLE `parameters` (
-	`id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	`item_id` INT(10) UNSIGNED NOT NULL,
-	`name` VARCHAR(128) NOT NULL,
-	`optional` BOOLEAN NOT NULL,
-	`type` VARCHAR(64) NOT NULL,
-	`html_desc` VARCHAR(256) NOT NULL,
-	`bbcd_desc` VARCHAR(256) NOT NULL,
-
-	INDEX (`item_id`),
-	FOREIGN KEY (`item_id`) REFERENCES `items` (`id`)
+	UNIQUE (`heading_id`, `order`),
+	FOREIGN KEY (`heading_id`) REFERENCES `headings` (`id`)
 );
