c27d55090c1dc9ecc20e7b35c700b5f133a3edd0
Raphael Mobis Tacla <r.mobis@gmail.com>
2012-12-15 17:07:50 -0200

    Renamed constants, added full_link attr to Item model.

--- application/models/item.php
+++ application/models/item.php
@@ -1,10 +1,10 @@
 <?php
 
 class Item extends Eloquent {
-	public static $VARIABLE	= 'Variable';
-	public static $FUNCTION	= 'Function';
-	public static $CONSTANT	= 'Constant';
-	public static $TYPE		= 'Type';
+	const TYPE_VARIABLE	= 'Variable';
+	const TYPE_FUNCTION	= 'Function';
+	const TYPE_CONSTANT	= 'Constant';
+	const TYPE_TYPE		= 'Type';
 
 	public function module() {
 		return $this->belongs_to('Module');
@@ -13,5 +13,31 @@ class Item extends Eloquent {
 	public function headings() {
 		return $this->has_many('Heading', 'item_id')
 					->order_by('order');
+	}
+
+	public function full_link() {
+		switch ($this->type) {
+			case Item::TYPE_VARIABLE:
+				$short = 'var';
+				break;
+
+			case Item::TYPE_FUNC:
+				$short = 'func';
+				break;
+
+			case Item::TYPE_CONSTANT:
+				$short = 'const';
+				break;
+
+			case Item::TYPE_TYPE:
+				$short = 'type';
+				break;
+
+			default:
+				$short = 'home';
+				break;
+		}
+
+		return URL::to_route($short, array($this->link));
 	}
 }
