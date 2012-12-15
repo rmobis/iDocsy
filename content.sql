a63268f77f96b676fc50429af192a02c673b234b
Raphael Mobis Tacla <r.mobis@gmail.com>
2012-12-15 14:55:51 -0200

    Rewrote and indexed test data.
    This is to reflect the new database structure changes.

--- content.sql
+++ content.sql
@@ -1,48 +1,60 @@
 INSERT INTO `modules`
-(`name`) VALUES
-('iBot Library');
+(`name`,			`link`) VALUES
+('iBot Library',	'ibot');
 
-INSERT INTO `sections`
-(`module_id`, `name`) VALUES
-(1, 'Variables'),
-(1, 'Functions'),
-(1, 'Types');
 
 INSERT INTO `items`
-(`section_id`, `name`, `link`) VALUES
-(1, 'cap', 'cap'),
-(1, 'exp', 'exp'),
-(1, 'hp', 'hp'),
-(1, 'hppc', 'hppc'),
-(1, 'axe', 'axe'),
-(1, 'axepc', 'axepc'),
-(1, 'club', 'club'),
-(1, 'clubpc', 'clubpc'),
-(2, 'attack', 'attack'),
-(2, 'levitate', 'levitate'),
-(2, 'moveitems', 'moveitems'),
-(2, 'npcsay', 'npcsay'),
-(2, 'time', 'time'),
-(2, 'ischannel', 'ischannel'),
-(2, 'windowcount', 'windowcount'),
-(2, 'itemcost', 'itemcost'),
-(2, 'itemid', 'itemid'),
-(3, 'Creature', 'creature'),
-(3, 'Item', 'item'),
-(3, 'Message', 'message');
+(`module_id`,	`type`,			`name`,			`link`) VALUES
 
-INSERT INTO `item_data`
-(`item_id`, `html_desc`, `bbcd_desc`, `html_usage`, `bbcd_usage`) VALUES
-(1, 'Your current capacity left', '', '<pre class="prettyprint linenums lang-lua">if cap < 10 then\n\t-- Less than 10 cap left\nend</pre>', ''),
-(9, 'Attacks given creature', '', '<pre class="prettyprint linenums lang-lua">attack(followed) -- Attacks the creature you\'re following</pre>', ''),
-(18, 'Holds information about a creature.', '', '<pre class="prettyprint linenums lang-lua">if (target.hppc < 10) then\n\tsay(\'exori con\')\nend</pre>', '');
+(1,				'Variable',		'cap',			'cap'),
+(1,				'Variable',		'exp',			'exp'),
+(1,				'Variable',		'hp',			'hp'),
+(1,				'Variable',		'hppc',			'hppc'),
+(1,				'Variable',		'axe',			'axe'),
+(1,				'Variable',		'axepc',		'axepc'),
+(1,				'Variable',		'club',			'club'),
+(1,				'Variable',		'clubpc',		'clubpc'),
+(1,				'Function',		'attack',		'attack'),
+(1,				'Function',		'levitate',		'levitate'),
+(1,				'Function',		'moveitems',	'moveitems'),
+(1,				'Function',		'npcsay',		'npcsay'),
+(1,				'Function',		'time',			'time'),
+(1,				'Function',		'ischannel',	'ischannel'),
+(1,				'Function',		'windowcount',	'windowcount'),
+(1,				'Function',		'itemcost',		'itemcost'),
+(1,				'Function',		'itemid',		'itemid'),
+(1,				'Type',			'Creature',		'creature'),
+(1,				'Type',			'Item',			'item'),
+(1,				'Type',			'Message',		'message');
 
-INSERT INTO `parameters`
-(`item_id`, `name`, `optional`, `type`, `html_desc`, `bbcd_desc`) VALUES
-(9, 'creature', false, '{Creature}', 'Creature to attack', '');
+INSERT INTO `headings`
+(`item_id`,		`order`,	`name`,			`html_content`) VALUES
 
-INSERT INTO `return_values`
-(`item_id`, `type`, `html_desc`, `bbcd_desc`) VALUES
-(1, '{number}', 'Capacity left', '');
+(1,				1, 			'Description',	'Your character\'s current left capacity.'),
+(2,				1, 			'Description',	'Your character\'s current experience points.'),
+(3,				1, 			'Description',	'Your character\'s current health points.'),
+(4,				1, 			'Description',	'Your character\'s current health point percentage.'),
+(5,				1, 			'Description',	'Your character\'s current axe skill.'),
+(6,				1, 			'Description',	'Your character\'s current axe skill\'s pecentage towards the next level.'),
+(7,				1, 			'Description',	'Your character\'s current club skill.'),
+(8,				1, 			'Description',	'Your character\'s current club skill\'s pecentage towards the next level.'),
+(9,				1,			'Description',	'Attacks a given <a href="/docs/type/creature">creature</a>.'),
+(9,				2,			'Usage',		'<pre class="prettyprint linenums lang-lua">attack(creature)</pre>'),
+(14,			1,			'Description',	'Checks whether given conversation channel is open.'),
+(14,			2,			'Usage',		'<pre class="prettyprint linenums lang-lua">ischannel(channel)</pre>');
 
 
+INSERT INTO `sub_headings`
+(`heading_id`,	`order`,	`name`,			`html_content`) VALUES
+
+(1,				1,			'Return Type',	'<span class="code">{number}</span>'),
+(2,				1,			'Return Type',	'<span class="code">{number}</span>'),
+(3,				1,			'Return Type',	'<span class="code">{number}</span>'),
+(4,				1,			'Return Type',	'<span class="code">{number}</span>'),
+(5,				1,			'Return Type',	'<span class="code">{number}</span>'),
+(6,				1,			'Return Type',	'<span class="code">{number}</span>'),
+(7,				1,			'Return Type',	'<span class="code">{number}</span>'),
+(8,				1,			'Return Type',	'<span class="code">{number}</span>'),
+(10,			1,			'Parameters',	'<ul><li><span class="code">creature - {Creature} - <a href="/docs/type/creature">Creature</a> to be attacked.</span></li></ul>'),
+(12,			1,			'Parameters',	'<ul><li><span class="code">channel - {string} - Channel to be checked.</span></li></ul>'),
+(12,			2,			'Returns',		'<span class="code">{boolean} - Wheter the checked channel is open.</span>');
